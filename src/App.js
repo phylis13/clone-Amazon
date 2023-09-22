3e1d891329c4640b9e433e91ad6fc94b51acb9ee
phylis13 <phylisnjeri54@gmail.com>
2023-09-22 23:08:27 +0300

    add new features in App.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// import Login from "./Login";
+// import Header from "./Header";
+// import Checkout from "./Checkout";
+// import Home from "./Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>hello Amazon</h1>
+        {/*localhost.com/ */}
+        {/*localhost.com/checkout */}
+        {/*localhost.com/login */}
+        
+
+        
+      </div>
+   
   );
 }
 
