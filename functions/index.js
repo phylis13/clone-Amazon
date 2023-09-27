9cfb8bd210ac7b7ad7d3d6dfb17765348339edf6
phylis13 <phylisnjeri54@gmail.com>
2023-09-27 21:56:37 +0300

    stipe api keys added

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(
-  "sk_test_51HPvU9DFg5koCdLGeOEiFvwHat4v8eMjX6SY0YCwxPBQBUPhKy1fPVhiSM5cQtgW7QBG9ydQcXnW57TDxVE2f3H000HSfmEQZF"
+  "sk_test_51NuNk1L33t8qDwgeUS1CJeBVS06CcpYLRz6YwxNaSpCNwOBKSXw2AWvHnpHnBs70SrFPQDSh7be8YmKSaBKWIzfS00PLTirsO4"
 );
 
 // API
--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
 const promise = loadStripe(
-  "pk_test_51HPvU9DFg5koCdLGJJbNo60QAU99BejacsvnKvT8xnCu1wFLCuQP3WBArscK3RvSQmSIB3N0Pbsc7TtbQiJ1vaOi00X9sIbazL"
+  "pk_test_51NuNk1L33t8qDwgeqqp0FSmXYD1YySN4P6FsITHOaFtrBuueTDDeWUhgTir2wBXUkKYG9gWJHIChEMInTKK9Yzu6005S5r1VIc"
 );
 
 function App() {
